af2ee4f06c816cbfa5e7e47c25aa0a9aca3bb2c9
HoorayTritium <ryuutomi1110@gmail.com>
2021-08-21 17:07:58 +0900

    api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
-  res.status(200).json({ name: 'John Doe' })
+export default function example(req, res) {
+  // console.log(req.headers);
+  const ip = req.client._server._connectionKey;
+  // await fetch(process.env.SS_POST_URL, {
+  //   method: "POST", // or 'PUT'
+  //   headers: {
+  //     "Content-Type": "application/x-www-form-urlencoded",
+  //   },
+  //   body: "ipAddress=" + ip,
+  // })
+  //   .then((response) => response.json())
+  //   .then((data) => {
+  //     console.log("Success:", data);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error:", error);
+  //   });
+
+  res.status(200).json({ status: "ok", ipAddress: ip });
 }
+
